cd9d8d8903862006d29c68d553f845abaddc301c
Yuwen <kfk07027@gmail.com>
2023-01-04 11:10:05 +0800

    feat: add hsin-chu parking data

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,7 +1,25 @@
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import styles from './Map.module.scss'
+import data from 'data/hcParkInfo.json'
 
 function Map() {
+  const filterDataList = data.filter((parking) => Number(parking.FREESPACE) > 0)
+
+  const dataList = filterDataList.map((parking) => (
+    <Marker
+      key={parking.PARKNO}
+      position={{
+        lat: Number(parking.X_COORDINATE),
+        lng: Number(parking.Y_COORDINATE),
+      }}
+    >
+      <Popup autoClose={false}>
+        <>
+          <p>剩餘車位: {parking.FREESPACE}</p>
+        </>
+      </Popup>
+    </Marker>
+  ))
   return (
     <MapContainer
       center={[24.809487, 120.974726]}
@@ -13,6 +31,7 @@ function Map() {
         maxZoom={18}
         attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
       />
+      {dataList}
     </MapContainer>
   )
 }
